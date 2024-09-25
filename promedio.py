1ba88ddc32a3097003e886152340ccead476fed3
brianasanchez <a.23308051281213@cbtis128.edu.mx>
2024-09-24 20:03:09 -0600

    Add files via upload

---
+++ promedio.py
@@ -0,0 +1,14 @@
+print(" ") # print imprime un espacio
+
+print("Briana Sarahi Sanchez Perez 3-w") 
+print(" ") # print imprime un espacio
+
+promedio1 = float(input("Ingrese el primer promedio : ")) # te pide que ingreses el primer promedio
+promedio2 = float(input("Ingrese el segundo promedio : ")) # te pide que ingreses el segundo promedio 
+promedio3 = float(input("Ingrese el tercer promedio : ")) # te pide que ingreses el tercer promedio 
+
+suma = (promedio1 + promedio2 + promedio3) / 3 # se realiza suma y divicion de los 3 promedios
+
+print("EL promedio es : ", suma) # se imprime resultado de la suma y la divicion
+
+print(" ") # print imprime un espacio6
